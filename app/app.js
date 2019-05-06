e3ac1ea45f225e5be7223dd300449454ed6b07c9
Shuhei Kagaya <shuhei.lab@gmail.com>
2019-05-06 22:07:31 +0900

    Initial commit.

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { corsMiddleware, customDefaultHeaderMiddleware, } = require('./middlewares');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(customDefaultHeaderMiddleware);
+app.use(corsMiddleware);
+app.use(router);
+
+module.exports = app;
---
+++ app/middlewares/index.js
@@ -0,0 +1,7 @@
+const { corsMiddleware } = require('./cors-middleware');
+const { customDefaultHeaderMiddleware } = require('./custom-default-header-middleware');
+
+module.exports = {
+  corsMiddleware,
+  customDefaultHeaderMiddleware,
+};
